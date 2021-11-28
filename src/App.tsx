abfc85ce208708b61596f9c11847559657570c75
Lucas Capelotto da silva <lcapelotto@gmail.com>
2021-11-28 09:31:06 -0300

    1.0

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import CarouselComponent from './components/carousel/CarouselComponent';
+
+
+function App() {
+  return (
+    <>
+      <CarouselComponent></CarouselComponent>
+    </>
+  );
+}
+
+export default App;
